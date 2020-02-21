f92685f6ec7be00bd2bec8b1c5131e017d2a33b2
Matthew Mahler <matthewjamesmahler@gmail.com>
2020-02-20 20:18:26 -0500

    oops

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: './src/images/ZDrecordingslogoblack.png',
+        icon: './src/images/ZDrecordingslogowhite.png',
         include_favicon: true, // Include favicon
       },
     },
